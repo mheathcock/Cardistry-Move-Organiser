8b2798bbfad754e816573661f178766e00a0370a
mheathcock <mheathcock@hotmail.co.uk>
2025-10-18 15:13:45 +0100

    Started work on polishing GUI design for login/register

---
+++ README.md
@@ -0,0 +1,31 @@
+# A desktop application to log, review and make notes on videos of Users cardistry creations.
+Built using **Python** with the built in ```tkinter``` library for the GUI and a custom backend for user authentication and data management.
+
+
+ ## Currently working on: 
+   * GUI
+       - [X] ***Skeleton*** GUI For Account Login / Register
+       - [ ] ***Skeleton*** GUI for Video Upload
+       - [ ] ***Skeleton*** GUI for Video Database
+       - [ ] ***Skeleton*** GUI for Video Viewing / Notes
+       -
+       - [ ] ***Polished*** GUI for Account Login / Register 
+       - [ ] ***Polished*** GUI for Video Upload
+       - [ ] ***Polished*** GUI for Video Database
+       - [ ] ***Polished*** GUI for Video Viewing / Notes
+   * Backend
+     - [X] Account Database
+     - [X] Account Authorisation
+     - [X] ***Skeleton*** Video Storage / Retrieval 
+     - [ ] Video Storage / Retrieval
+
+  
+### Future Development
+To be worked on once core features are implemented
+
+* **Functionality** 
+  - **Categorisation Logic** backend and frontend to assign and filter for categories such as: 
+    - **Packet Cuts**, **Fans**, **Isolations** etc
+     - Sub-categories: **One Handed**, **Two Handed**, **Body** etc
+  - **Progress Tracking** System to allow users to mark moves as ***Learning***, ***Mastered***, ***Archived***
+  - **Search functionality** for move name and notes
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from tkinter import messagebox 
 from backend.auth import login_User, register_User
+import customtkinter as CTk
 
 
 
-app = Tk()
+app = CTk.CTk()
 app.geometry("1920x1080")
+app.configure(bg="#121212")
+app.state('zoomed')  
 
 """
 Front end GUI for user authentication.
@@ -14,14 +17,37 @@ def login_or_register_gui():
     #forget previous widgets
     for widget in app.winfo_children():
         widget.destroy()
-    app_frame = Frame(app)
+    
+  
+    app_frame = CTk.CTkFrame(app, fg_color="transparent")
     app_frame.pack(expand=True,anchor="center")
 
     app.title("Login / Register")
-    button = Button(app_frame, text="Login", command=login_gui, width=20, height=2)
-    button.pack(pady=10)
-    button = Button(app_frame, text="Register", command=register_gui, width=20, height=2)
-    button.pack(pady=10)    
+    
+   
+    CTk.CTkButton(
+        app_frame, 
+        text="Login", 
+        command=login_gui, 
+        width=250, 
+        height=50, 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
+        corner_radius=30,
+        fg_color="#BB86FB", 
+        hover_color="#9C5AF7"
+    ).pack(pady=10)
+    
+    CTk.CTkButton(
+        app_frame, 
+        text="Register", 
+        command=register_gui, 
+        width=250, 
+        height=50, 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
+        corner_radius=30,
+        fg_color="#BB86FB", 
+        hover_color="#9C5AF7"
+    ).pack(pady=10) 
 
 def login_gui():
        #forget previous widgets
@@ -29,43 +55,98 @@ def login_gui():
         widget.destroy()
     
     app.title("Login")
-    Button(app, text="Back", width=20, height=2, command=login_or_register_gui).pack(side="top", anchor="nw", pady=10, padx=10)#Back button to return to login/register choice screen
-    Label(app, text="Login", font=("Arial", 30)).pack() # Page Title
-    login_frame = Frame(app) # Frame to hold login form elements
-    login_frame.pack(expand=True,anchor="center") # Center the frame in the window
+    
+    CTk.CTkButton(
+        app, 
+        text="Back", 
+        width=100, 
+        height=30, 
+        command=login_or_register_gui,
+        fg_color="gray", 
+        hover_color="darkgray"
+    ).pack(side="top", anchor="nw", pady=10, padx=10)#Back button to return to login/register choice screen
+    main_Frame = CTk.CTkFrame(
+        master=app,
+        fg_color="#BB86FB",
+        corner_radius=150,
+        border_width=0
+    )
+    main_Frame.pack(expand=True, fill='x', anchor="center", padx=500)
 
+    login_frame = CTk.CTkFrame(
+        master = main_Frame,
+        fg_color="transparent"
+        ) 
+    # Center the frame in the window
+    login_frame.pack(expand=True, fill="both", padx=50, pady=50)
+    
+
+    # Page Title
+    CTk.CTkLabel(
+        master=login_frame, 
+        text="LOGIN", 
+        font=CTk.CTkFont(family="Summer Dream Sans Demo", size=60, weight="bold"),
+        text_color="white"
+    ).pack(pady=(40, 40))
 
     #Create two labels, Username and Password, two entry fields for input 
-    Label(login_frame, text="Username", font=("Arial", 14)).pack(pady=10)
-    e1 = Entry(login_frame,width=30)
-    e1.pack(pady=5)
+    username_entry = CTk.CTkEntry(
+    master=login_frame,
+    placeholder_text="Username",
+    font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="normal"),
+    width=450,
+    height=60,
+    corner_radius=30, 
+    fg_color="white",
+    text_color="#171717",
+    border_width=0
+    )
+    username_entry.pack(pady=(20, 10), padx=50)
 
-    Label(login_frame, text="Password", font=("Arial", 14)).pack(pady=10)
-    e2 = Entry(login_frame, width=30, show="*")
-    e2.pack(pady=5)
+    password_entry = CTk.CTkEntry(
+    master=login_frame,
+    placeholder_text="Password",
+    font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="normal"),
+    show="*", # Hides the text as dots for a password field
+     width=450,
+    height=60,
+    corner_radius=30, 
+    fg_color="white",
+    text_color="#171717",
+    border_width=0
+    )
+    password_entry.pack(pady=60, padx=50)
 
 
 #PRINT STATEMENTS FOR DEBUGGING
-    username = e1.get()
-    password = e2.get()
+    username = username_entry.get()
+    password = password_entry.get()
     print(f"Username: {username}, Password: {password}")
 
     #Function to handle login result
     def login_result():
-        if login_User(e1.get(), e2.get()):
+        if login_User(username_entry.get(), password_entry.get()):
             print("Login successful")
             upload_video_gui()
         else:
             print("Login failed")
             messagebox.askretrycancel("Login Fail", "Username or Password Incorrect. Try again?") 
-            e1.delete(0, END)
-            e2.delete(0, END)   
+            username_entry.delete(0, END)
+            password_entry.delete(0, END)   
 
     #Login button that calls the login_result function when clicked
-    Button(login_frame, text="Login", 
-           width=20, height=2, 
-           command=lambda: login_result()
-           ).pack(pady=20)
+    CTk.CTkButton(
+        login_frame, 
+        text="SUBMIT",
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=40, weight="bold"),
+        text_color="white",
+        width=200, 
+        height=50, 
+        corner_radius=35,
+        fg_color="black", 
+        hover_color="#333333",
+        command=login_result
+    ).pack(pady=(20, 5))
   
 
 
@@ -75,46 +156,101 @@ def register_gui():
         widget.destroy()
     app.title("Register")
 
-    Button(app, text="Back", width=20, height=2, command=login_or_register_gui).pack(side="top", anchor="nw", pady=10, padx=10) #Back button to return to login/register choice screen
-    Label(app, text="Register", font=("Arial", 30)).pack()# Page Title
+    #Back button to return to login/register choice screen
+    CTk.CTkButton(
+        app, 
+        text="Back", 
+        width=100, 
+        height=30, 
+        command=login_or_register_gui,
+        fg_color="gray", 
+        hover_color="darkgray"
+    ).pack(side="top", anchor="nw", pady=10, padx=10)
+    main_Frame = CTk.CTkFrame(
+        master=app,
+        fg_color="#BB86FB",
+        corner_radius=150,
+        border_width=0
+    )
+    main_Frame.pack(expand=True, fill='x', anchor="center", padx=500)
 
-    #Frame to hold register form elements
-    register_frame = Frame(app)
-    register_frame.pack(expand=True,anchor="center")
+    register_frame = CTk.CTkFrame(
+        master = main_Frame,
+        fg_color="transparent"
+        ) 
+    #center the frame in the window
+    register_frame.pack(expand=True, fill="both", padx=50, pady=50)
+    
+
+    #page Title
+    CTk.CTkLabel(
+        master=register_frame, 
+        text="REGISTER", 
+        font=CTk.CTkFont(family="Summer Dream Sans Demo", size=60, weight="bold"),
+        text_color="white"
+    ).pack(pady=(40, 40))
+
+    #create two labels, Username and Password, two entry fields for input 
+    username_entry = CTk.CTkEntry(
+    master=register_frame,
+    placeholder_text="Username",
+    font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="normal"),
+    width=450,
+    height=60,
+    corner_radius=30, 
+    fg_color="white",
+    text_color="#171717",
+    border_width=0
+    )
+    username_entry.pack(pady=(20, 10), padx=50)
 
 
-    #Create two labels, Username and Password, two entry fields for user input
-    Label(register_frame, text="Username", font=("Arial", 14)).pack(pady=10)
-    e1 = Entry(register_frame,width=30)
-    e1.pack(pady=5)
+    password_entry = CTk.CTkEntry(
+    master=register_frame,
+    placeholder_text="Password",
+    font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="normal"),
+    show="*", #hides the text as dots for a password field
+     width=450,
+    height=60,
+    corner_radius=30, 
+    fg_color="white",
+    text_color="#171717",
+    border_width=0
+    )
+    password_entry.pack(pady=60, padx=50)
 
-    Label(register_frame, text="Password", font=("Arial", 14)).pack(pady=10)
-    e2 = Entry(register_frame, width=30, show="*")
-    e2.pack(pady=5)
 
-##PRINT STATEMENTS FOR DEBUGGING
-    username = e1.get()
-    password = e2.get()
+#PRINT STATEMENTS FOR DEBUGGING
+    username = username_entry.get()
+    password = password_entry.get()
     print(f"Register Username: {username}, Register Password: {password}")
 
-#Function to handle registration result
+  #Function to handle registration result
     def register_result():
-        if register_User(e1.get(), e2.get()):
+        if register_User(username_entry.get(), password_entry.get()):
             print("Registration successful")
             messagebox.showinfo("Registration Success", "User registered successfully. Please login.")
             login_gui()
         else:
             print("Registration failed")
             messagebox.showerror("Registration Fail", "Username already exists. Try a different one.") 
-            e1.delete(0, END)
-            e2.delete(0, END)
-    #Register button that calls the register_result function when clicked
-    Button(register_frame, text="Login", 
-           width=20, height=2, 
-           command=lambda: register_result()
-           ).pack(pady=20)
-    
+            username_entry.delete(0, END)
+            password_entry.delete(0, END)  
 
+    #Login button that calls the login_result function when clicked
+    CTk.CTkButton(
+        register_frame, 
+        text="SUBMIT",
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=40, weight="bold"),
+        text_color="white",
+        width=200, 
+        height=50, 
+        corner_radius=35,
+        fg_color="black", 
+        hover_color="#333333",
+        command=register_result
+    ).pack(pady=(20, 5))
+  
 
 
 
